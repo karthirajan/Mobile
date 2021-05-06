d15f17e6b519c0f3ee07ef027bea1080bd3990b4
karthiS1007 <karthick@centricsoftware.com>
2021-05-06 20:56:40 +0530

    mobile

---
+++ src/test/java/org/mobiledemo/MobileTesting.java
@@ -0,0 +1,26 @@
+package org.mobiledemo;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class MobileTesting {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		File f = new File(".//app//android_9-28_minAPI28(nodpi)_apkmirror.com.apk"); // app location
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel"); 
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2"); //XCUITEST
+		cap.setCapability(MobileCapabilityType.APP, f.getAbsolutePath()); 
+		cap.setCapability(MobileCapabilityType.FULL_RESET, true);
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap); 
+		
+	
+	}
+}
